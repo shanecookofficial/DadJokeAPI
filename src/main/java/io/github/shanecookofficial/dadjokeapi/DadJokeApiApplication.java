a21980d5a3823b256431c7a2f5126e21ce2530d5
shanecookofficial <shanecookofficial@gmail.com>
2025-03-26 15:37:35 -0500

    Initial commit of DadJokeAPI

---
+++ src/main/java/io/github/shanecookofficial/dadjokeapi/DadJokeApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.shanecookofficial.dadjokeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DadJokeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DadJokeApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/shanecookofficial/dadjokeapi/DadJokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shanecookofficial.dadjokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DadJokeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
